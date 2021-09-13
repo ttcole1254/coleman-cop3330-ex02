a2f0fa9f83d33b78ba0d13a211f906fc64ba3526
Tyler Coleman <ttcole1254@gmail.com>
2021-09-12 20:21:13 -0400

    Added assignment code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,26 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import java.net.Authenticator;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Tyler Coleman
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner askInput = new Scanner(System.in);
+        System.out.println( "What is the input string?" );
+
+        String input = askInput.nextLine();
+        if(input.isEmpty()) {
+            System.out.println("Nothing was entered, please try again!");
+        }
+        else {
+            int inputChar = input.length();
+            System.out.println(input + " has " + inputChar + " characters including spaces.");
+        }
     }
 }
